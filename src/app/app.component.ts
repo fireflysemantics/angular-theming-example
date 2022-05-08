6b6561fc28d93df361e0431ba9d95c862ed87dc6
Ole Ersoy <ole.ersoy@gmail.com>
2022-05-07 21:25:23 -0400

    Added Theme Switchign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { StateService } from './services/state.service';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +9,48 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  public themeSelect = new FormControl();
+  themes: string[] = ['Light', 'Dark'];
+  themeClass:string = 'light-theme'
+  themeMap:Map<string,String> = new Map()
+
   constructor(
+    public s:StateService,
     private overlayContainer: OverlayContainer
-  ) {}
+  ) {
+    this.themeMap.set('Light', 'light-theme')
+    this.themeMap.set('Dark', 'dark-theme')
+  }
 
-  public toppings = new FormControl();
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  ngOnInit(): void {
+    this.themeSelect.valueChanges.subscribe(theme=>{
+      this.s.OS.put(this.s.OS.S.theme, this.themeMap.get(theme))
+    })
+    this.removeThemeClasses()
+    this.addThemeClass()
+    this.s.OS.S.theme.obs.subscribe(t=>console.log(t))
+  }
 
-  themeClass:string = 'my-theme'
-  ngOnInit(): void {    
-    // remove old theme class and add new theme class
-    // we're removing any css class that contains '-theme' string but your theme classes can follow any pattern
+
+  /**
+   * Remove css classes that contain the classPostfix
+   * string from the CDK overlayContainer.
+   */
+  removeThemeClasses(classPostfix:string = '-theme') {
     const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
-    const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme'));
+    const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) => item.includes(classPostfix));
     if (themeClassesToRemove.length) {
        overlayContainerClasses.remove(...themeClassesToRemove);
-    }
-//    overlayContainerClasses.add(this.themeClass);
-    this.overlayContainer.getContainerElement().classList.add('my-theme')
-    console.log(this.overlayContainer.getContainerElement().classList)
+    }    
   }
-}
 
+  /**
+   * Add the themeClass to the overlay container class list.
+   * @param themeClass The theme class to add
+   */
+  addThemeClass(themeClass:string = 'light-theme') {
+    this.overlayContainer.getContainerElement().classList.add(themeClass)
+  }
+
+}
